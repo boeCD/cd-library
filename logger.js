0d210efcb6279c46ea83e8e62f4b9194e6e1e475
boeCD <128589797+boeCD@users.noreply.github.com>
2024-09-05 09:50:11 +0200

    Update logger.js
    extended with other common methods

--- logger.js
+++ logger.js
@@ -1,9 +1,30 @@
 const logger = {
     log: function(message) {
+        this._checkAndLog("log", message);
+    },
+    info: function(message) {
+        this._checkAndLog("info", message);
+    },
+    warn: function(message) {
+        this._checkAndLog("warn", message);
+    },
+    error: function(message) {
+        this._checkAndLog("error", message);
+    },
+    debug: function(message) {
+        this._checkAndLog("debug", message);
+    },
+    _checkAndLog: function(type, message) {
         const body = localStorage.getItem("cd-debug");
-        // Check if the body has the attribute cd-debug="true"
         if (body) {
-            console.log("🐙🐙🐙🐙 ",message);
+            console[type]("🐙🐙🐙🐙 ", message);
         }
     }
 };
+
+// Example usage:
+logger.log("This is a log message.");
+logger.warn("This is a warning message.");
+logger.error("This is an error message.");
+logger.info("This is an info message.");
+logger.debug("This is a debug message.");
